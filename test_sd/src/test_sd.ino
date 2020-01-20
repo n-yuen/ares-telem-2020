b17e65f0be39252187d8bc513c8662994d9106dc
nyuen <nyuen@g.ucla.edu>
2020-01-19 17:15:08 -0800

    added sd test

--- test_sd/src/test_sd.ino
+++ test_sd/src/test_sd.ino
@@ -3,21 +3,20 @@
 //#include <RH_RF95.h>
 #include <SPI.h>
 #include <TinyGPS++.h>
-#include <SdFat.h>
+#include <SD.h>
 #define DEBUG 1
 #define FT_PER_M 0.304800609601
 #define DELAY_INTERVAL 1000
-#define SD_CHIPSEL 10
+#define SD_CHIPSEL 9
 
 // Singleton instance of the radio driver
 //RH_RF95 rf95(RFM95_CS, RFM95_INT);
-#define gpsSerial Serial1
+// #define gpsSerial Serial1
 
 
 TinyGPSPlus gps;
 
-SdFat sdCard;
-SdBaseFile saveFile;
+File saveFile;
 
 
 uint16_t highest_alt = 0;
@@ -31,31 +30,16 @@ struct gpsCoordinate {
     uint16_t lng;
 } coord;
 
-
-void printFormatted(uint16_t coordVal) {  // Prints decimal value
-    uint32_t adjustedVal = (uint32_t)coordVal << 16;
-    char toPrint[10];
-    sprintf(toPrint, "%09lu", adjustedVal);  // Pad with zeroes
-    toPrint[5] = '\0';                       // Truncate to 5 sigfigs
-    Serial.print(toPrint);
-}
 uint8_t tx_buf[sizeof(coord)];
 void setup() {
 
-    Serial.println(sdCard.card()->errorCode());
-    if(!sdCard.begin(SD_CHIPSEL)) {
-        if(sdCard.card()->errorCode()) {
-            Serial.print("Error code: ");
-            Serial.println(sdCard.card()->errorCode(), HEX);
-            Serial.println(sdCard.card()->errorData());
-        }
-        else {
-            Serial.println("need a reformat");
-        }
-        return;
+    Serial.begin(115200);
+
+    if(!SD.begin(SD_CHIPSEL)) {
+        Serial.println("Failed");
     }
     Serial.print("Trying to create file: ");
-    if(saveFile.open("./output.txt", O_CREAT | O_WRITE | O_TRUNC)) {
+    if(saveFile = SD.open("./output.txt", FILE_WRITE)) {
         Serial.println("Created file");
         return;
     }
@@ -74,34 +58,13 @@ void setup() {
         }
     }
     delay(100);
-    Serial.println("yay");
-    gpsSerial.begin(9600);
+    saveFile.write("1");
+    saveFile.write("2");
+
+    saveFile.close();
 
 }
 
 void loop() {
-    int m;
-    uint32_t lat, lng;
-    //Serial.print("loop");
-    if (gpsSerial.available()) {
-        int x = gpsSerial.read();
-        
-        gps.encode(x);
-        
-        if (gps.location.isUpdated()) {
-            
-            unsigned long currentMillis = millis();
-            if (currentMillis - previousMillis > DELAY_INTERVAL) {
 
-                previousMillis = currentMillis;
-                Serial.print("Lat = ");
-                Serial.println(gps.location.lat(), 6);
-                Serial.print("Lng = ");
-                Serial.println(gps.location.lng(), 6);
-
-                saveFile.write(gps.location.lat());
-                saveFile.write(gps.location.lng());
-            }
-        }
-    }
 }
